bfa7338f0d439ed369c0943ba2d728999a8e2123
Daniela Butano <daniela@intermine.org>
2018-06-19 10:48:17 +0100

    fixed R basic functions

--- R/basicTutorial.R
+++ R/basicTutorial.R
@@ -1,30 +1,31 @@
+#setlibrary
 library(InterMineR)
 # 1.show all mines
 listMines()
+im<-initInterMine(listMines()["HumanMine"])
+listDatasets(im)
+getRelease(im)
 
 # 2.show all templates
-im<-initInterMine(listMines()["HumanMine"])
 getTemplates(im)
 
 # 3.display given template
-queryGeneOrth <- getTemplateQuery(
-  im = im,
-  name = "Gene_Orth")
-queryGeneOrth
+template <- getTemplateQuery(im, "Gene_Orth")
+template
 
 # 4. run it
-resGeneOrth <- runQuery(im, queryGeneOrth)
-resGeneOrth
+head(runQuery(im, template))
 
 # 5 Update the gene constraint and run the template again
-queryGeneOrth$where = setConstraints(
-  modifyQueryConstraints = queryGeneOrth,
+template$where = setConstraints(
+  modifyQueryConstraints = template,
   m.index = 1,
   values = list("ABO")
 )
+head(runQuery(im, template))
+# OR modify directly the value of the first constraint from the list query
+# template$where[[1]]$value="ABO"
 
-resGeneOrth <- runQuery(im, queryGeneOrth)
-resGeneOrth
 
 # 6 add a new constraint
 newConstraint <- list(
@@ -33,19 +34,21 @@ newConstraint <- list(
   value=c("H. sapiens"), 
   code=c("B")
 )
-queryGeneOrth$where[[2]] <- newConstraint
-queryGeneOrth$where
-resGeneOrth <- runQuery(im, queryGeneOrth)
-resGeneOrth
+template$where[[2]] <- newConstraint
+head(runQuery(im, template))
 
 #. 7 add a new column
-queryGeneOrth.InterMineR = setQuery(
-  inheritQuery = queryGeneOrth,
-  select = c(queryGeneOrth$select, 
+query = setQuery(
+  inheritQuery = template,
+  select = c(template$select, 
              "Gene.homologues.dataSets.name")
 )
 
-getSelect(queryGeneOrth.InterMineR)
-resGeneOrth <- runQuery(im, queryGeneOrth.InterMineR)
-resGeneOrth
+getSelect(query)
+head(runQuery(im, query))
 
+#OR
+template$select[[6]] <- "Gene.homologues.dataSets.name"
+template$select[[7]] <- "Gene.homologues.dataSets.url"
+template$select
+head(runQuery(im, template))
